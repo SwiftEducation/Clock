115ec8a612db2b0bc6c500955e5d432b91f419e3
Bishal Ghimire <bishal.ghimire@gmail.com>
2015-07-14 16:01:04 +0545

    Comments Added Comments Added to Clock Model and ViewController Class

--- Clock/Clock.swift
+++ Clock/Clock.swift
@@ -8,7 +8,12 @@ This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAl
 import Foundation
 
 class Clock {
-    
+    // currentTime  is a computed property
+    // In addition to stored properties, classes, structures, and enumerations can define
+    // computed properties, which do not actually store a value. Instead, they provide a 
+    // getter and an optional setter to retrieve and set other properties and values indirectly.
+    // Refer - https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Properties.html#//apple_ref/doc/uid/TP40014097-CH14-ID259
+
     var currentTime: NSDate {
         return NSDate()
     }
